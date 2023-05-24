af22efbbcaea1ce0fb3eed15a6fa2c67bd549db8
Lại Ngọc Sơn <ggdriversfin01@gmail.com>
2023-05-24 15:38:03 +0700

    commit code
    http://localhost:8080/api

--- out/artifacts/quanlysv_war_exploded/assets/js/admin/add_form.js
+++
@@ -1,9 +0,0 @@
-function showModal(elementId){
-    const el = document.getElementById(elementId);
-    el.classList.add('hien-thi-modal')
-}
-
-function hideModal(elementId){
-    const el = document.getElementById(elementId);
-    el.classList.remove('hien-thi-modal')
-}
